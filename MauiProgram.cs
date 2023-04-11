98f69a328c4208e9fe8d8d46246f21f695525b23
Theodor Slotte Norman <theodor.norman@gmail.com>
2023-04-11 19:37:23 +0200

    Add project files.

---
+++ MauiProgram.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+using MauiApp1.Data;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace MauiApp1;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			});
+        builder.Services.AddAuthorizationCore();
+        builder.Services.AddScoped<CustomAuthenticationStateProvider>();
+        builder.Services.AddScoped<AuthenticationStateProvider>(s => s.GetRequiredService<CustomAuthenticationStateProvider>());
+
+        builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+		builder.Services.AddBlazorWebViewDeveloperTools();
+		builder.Logging.AddDebug();
+#endif
+
+		builder.Services.AddSingleton<WeatherForecastService>();
+
+		return builder.Build();
+	}
+}
